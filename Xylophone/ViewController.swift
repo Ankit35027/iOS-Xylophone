2dd6eea2117d936ea78b1d74e0f48bbca6838479
Ankit <ankitgarg35027@gmail.com>
2025-11-02 22:09:14 +0530

    My first Dynamic ios project in one go

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -5,17 +5,83 @@
 //  Created by Angela Yu on 28/06/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func KeyPressed(_ sender: UIButton) {
+        sender.alpha = 0.5
 
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                sender.alpha = 1.0
+            }
+        playSound(SOUND: sender.currentTitle!)
+        print(sender.currentTitle as Any )
+    }
     
-
+    func playSound(SOUND: String) {
+        let url = Bundle.main.url(forResource: SOUND, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
+
+//import UIKit
+//import AVFoundation
+//var player: AVAudioPlayer?
+//
+//class ViewController: UIViewController {
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+//
+//    @IBAction func KeyPressed(_ sender: UIButton) {
+//        print("I got Pressed ")
+//        playSound()
+//    }
+////    func playSound() {
+////        guard let path = Bundle.main.path(forResource: "C", ofType:"wav") else {
+////            return }
+////        let url = URL(fileURLWithPath: path)
+////
+////        do {
+////            player = try AVAudioPlayer(contentsOf: url)
+////            player?.play()
+////            
+////        } catch let error {
+////            print(error.localizedDescription)
+////        }
+////    }
+//    func playSound() {
+//        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
+//
+//        do {
+//            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+//            try AVAudioSession.sharedInstance().setActive(true)
+//            
+//            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+//
+//
+//            guard let player = player else { return }
+//
+//            player.play()
+//
+//        } catch let error {
+//            print(error.localizedDescription)
+//        }
+//    }
+//    
+//    
+//
+//}
 
